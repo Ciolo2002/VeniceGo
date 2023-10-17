10677426af09a1819296c3ab4bd97cc97d9b1c2e
michael <michael@busnet.it>
2023-10-18 00:18:41 +0200

    Connessione a database

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
