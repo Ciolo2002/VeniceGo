37af5163103d2829c49b4c5629592a93573b8fd9
Tommaso <soncintommaso@gmail.com>
2023-11-11 18:02:38 +0100

    done: downlaod actv data

--- lib/navigation_data/navigation_data_downloader.dart
+++ lib/navigation_data/navigation_data_downloader.dart
@@ -1,16 +1,18 @@
 import 'dart:io';
-import 'package:venice_go/permission_request.dart';
+import 'package:http/http.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NavigationDataDownloader {
-  /// Download the navigation data from the ACTV website and saves it in the app's local storage.
+  /// Downloads the navigation data from the ACTV website and saves it in the app's local storage.
+  /// Assumes that permissions are already granted at app start.
   /// Throws an exception if the download fails or the permissions are not granted.
   Future<void> download() async {
-    var url = Uri.https("actv.avmspa.it",
+    // TODO: call this method every two weeks
+    Uri url = Uri.https("actv.avmspa.it",
         "sites/default/files/attachments/opendata/navigazione/actv_nav.zip");
-    var client = HttpClient();
-    // Download zip file from url
-    var request = await client.getUrl(url);
-    var response = await request.close();
-    await PermissionRequest.request();
+    Response response = await get(url);
+    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+    File actvData = File("${appDocumentsDir.path}/actv_nav.zip");
+    actvData.writeAsBytes(response.bodyBytes);
   }
 }
