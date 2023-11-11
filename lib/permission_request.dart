76c1e2c03e18339604efd5ec20ee5b82ea44a6a9
Tommaso <soncintommaso@gmail.com>
2023-11-11 16:37:27 +0100

    refactor: reqeust method of PermissionRequest class.

--- lib/permission_request.dart
+++ lib/permission_request.dart
@@ -1,14 +1,27 @@
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:io' show Platform;
 
 class PermissionRequest {
+  static Future<void> _requestAndroid() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.location,
+      Permission.storage,
+    ].request();
+    // Check if permissions are granted
+    statuses.forEach((permission, status) {
+      if (!status.isGranted) {
+        throw Exception("Please give permissions.");
+      }
+    });
+  }
+
+  /// Requests the permissions for storage and location.
   static Future<void> request() async {
-    var storage = await Permission.storage.status;
-    while (storage.isDenied) {
-      await Permission.storage.request();
-    }
-    var location = await Permission.location.status;
-    while (location.isDenied) {
-      await Permission.location.request();
+    if (Platform.isAndroid) {
+      await _requestAndroid();
+    } else if (Platform.isIOS) {
+      // TODO iOS
+      // https://stackoverflow.com/questions/68599765/flutter-permission-handler-grant-not-showing-on-ios
     }
   }
 }
