afcc04fd904b30652a9dd381e3dcd1046ecdcc4a
Tommaso <soncintommaso@gmail.com>
2023-11-16 21:16:50 +0100

    fix: download of data now properly works

--- lib/navigation_data/navigation_data_downloader.dart
+++ lib/navigation_data/navigation_data_downloader.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import "package:archive/archive_io.dart";
 
@@ -7,47 +7,33 @@ class NavigationDataDownloader {
   /// Downloads the navigation data from the ACTV website and saves it in the app's local storage.
   /// Assumes that permissions are already granted at app start.
   /// Throws an exception if the download fails or data is corrupted.
-  Future<void> _download() async {
+  Future<void> _download(final Directory dir) async {
     // TODO: call this method every two weeks
-    // TODO: find out why it does not download full file
-    const String url =
-        "https://actv.avmspa.it/sites/default/files/attachments/opendata/navigazione/actv_nav.zip";
-    Response response = await get(Uri.parse(url));
-    if (response.statusCode != 200) {
-      throw Exception("Download failed, try again later.");
-    }
-    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
-    File actvData = File("${appDocumentsDir.path}/actv_nav.zip");
-    actvData.writeAsBytes(response.bodyBytes);
-    final int contentLength = response.contentLength ?? 0;
-    final int downloadedSize = await actvData.length();
-
-    if (contentLength != downloadedSize) {
-      await actvData.delete();
-      throw Exception("Downloaded file is corrupted or incomplete.");
+    final Uri uri = Uri.https("actv.avmspa.it",
+        "sites/default/files/attachments/opendata/navigazione/actv_nav.zip");
+    // Download using Request and Response
+    final http.Request request = http.Request('GET', uri);
+    final http.StreamedResponse response = await request.send();
+    final File file = File("${dir.path}/actv_nav.zip");
+    await response.stream.pipe(file.openWrite());
+    // Check for file integrity
+    if (response.contentLength != file.lengthSync()) {
+      throw Exception("Downloaded file is corrupted.");
     }
   }
 
   /// Extracts the navigation data from the app's local storage.
   /// Assumes that permissions are already granted at app start.
   /// Throws an exception if the unzipping fails.
-  Future<void> _extractData() async {
-    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
-    final String zipFile = "${appDocumentsDir.path}/actv_nav.zip";
+  Future<void> _extractData(final Directory dir) async {
+    final String zipFile = "${dir.path}/actv_nav.zip";
 
     try {
       final File file = File(zipFile);
       final archive = ZipDecoder().decodeBytes(file.readAsBytesSync());
-      extractArchiveToDisk(archive, 'out');
+      extractArchiveToDisk(archive, 'actv_nav');
     } catch (e) {
-      // Handle the FormatException
-      if (e is FormatException) {
-        // The ZIP file is corrupted or incomplete
-        print("Error: Invalid ZIP file format or incomplete archive.");
-      } else {
-        // Other unexpected errors
-        print("Error: Failed to extract ZIP archive: $e");
-      }
+      rethrow;
     }
   }
 
@@ -56,8 +42,8 @@ class NavigationDataDownloader {
   /// Throws an exception if the download or the unzipping fails.
   Future<void> initNavigationData() async {
     try {
-      await _download();
-      await _extractData();
+      await _download(await getApplicationDocumentsDirectory());
+      await _extractData(await getApplicationDocumentsDirectory());
     } catch (e) {
       rethrow;
     }
