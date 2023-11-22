14c16e7765d0e00e5e56dfb66203d9d022e30e75
Tommaso <soncintommaso@gmail.com>
2023-11-22 23:17:43 +0100

    removed navigation data, not used anymore

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    NavigationData navigationData = NavigationData();
-    navigationData.initNavigationData();
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
