5c12fb59007e81c4f757b7299fd8a22f5f7f37b0
Tommaso <soncintommaso@gmail.com>
2023-11-24 12:38:51 +0100

    add: optional parameter to GoogleMaps, it's now possible to pass markers that are added on the map

--- lib/pages/google_maps.dart
+++ lib/pages/google_maps.dart
@@ -13,7 +13,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../locations.dart' as locations;
 
 class GoogleMaps extends StatefulWidget {
-  const GoogleMaps({super.key});
+  final Map<String, Marker>? markers;
+  const GoogleMaps({super.key, this.markers});
 
   @override
   State<GoogleMaps> createState() => _MyGoogleMapsState();
@@ -22,31 +23,43 @@ class GoogleMaps extends StatefulWidget {
 class _MyGoogleMapsState extends State<GoogleMaps> {
   late GoogleMapController mapController;
 
-  final LatLng _veniceGeoCoords =
-      const LatLng(45.4371908, 12.3345898); //COORDINATE DI VENEZIA
+  final LatLng _veniceGeoCoords = const LatLng(45.4371908, 12.3345898);
 
   final Map<String, Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.markers != null) {
+      setState(() {
+        _markers.addAll(widget.markers!);
+      });
+    }
+  }
 
   /*
     * QUESTO CI PERMETTE DI DEFINIRE DEI PUNTI DI INTERESSE CHE VOGLIAMO NOI SULLA MAPPA
     * per vedere il loro funzionamento (con gli esempi che da google, usare le coordiante
     * TODO CAPIRE COME FARE A CAMBIARE I PUNTI DI INTERESSE
+    * (guardare commenti in locations_search_screen.dart)
   */
   Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
-    setState(() {
-      for (final office in googleOffices.offices) {
-        final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
-          infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
-          ),
-        );
-        _markers[office.name] = marker;
-      }
-    });
+    if (widget.markers == null) {
+      final googleOffices = await locations.getGoogleOffices();
+      setState(() {
+        for (final office in googleOffices.offices) {
+          final marker = Marker(
+            markerId: MarkerId(office.name),
+            position: LatLng(office.lat, office.lng),
+            infoWindow: InfoWindow(
+              title: office.name,
+              snippet: office.address,
+            ),
+          );
+          _markers[office.name] = marker;
+        }
+      });
+    }
   }
 
   @override
