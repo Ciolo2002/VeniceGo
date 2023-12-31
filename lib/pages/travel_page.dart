4bdf34b1cbb89c5f6af5b8eaa791ca1b98321ec5
Tommaso <soncintommaso@gmail.com>
2023-12-31 17:45:52 +0100

    working on calculating the points from start to end ("il percorso")

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -2,6 +2,7 @@ import "dart:async";
 import "dart:convert";
 import "package:flutter/material.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart" show dotenv;
+import "package:flutter_polyline_points/flutter_polyline_points.dart";
 import "package:geolocator/geolocator.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
 import "package:http/http.dart" as http;
@@ -18,7 +19,7 @@ class _TravelPageState extends State<TravelPage> {
   final Completer<GoogleMapController> _mapsController = Completer();
   late List<LatLng> _locations;
   late final LatLng _currentUserPosition;
-
+  late Set<Marker> _markers;
   @override
   void initState() async {
     super.initState();
@@ -32,6 +33,8 @@ class _TravelPageState extends State<TravelPage> {
     return LatLng(currentUserLocation.latitude, currentUserLocation.longitude);
   }
 
+  /// Sets the [_locations] variable to the geographical coordinates
+  /// of all the places ID given to the widget
   Future<List<LatLng>> _getPlacesfromPlaceID(
       List<String> destinationsID) async {
     String apiKey = dotenv.env["GOOGLE_MAPS_API_KEY"] as String;
@@ -49,18 +52,35 @@ class _TravelPageState extends State<TravelPage> {
       // Conversion from JSON to Dart objects
       dynamic tempJson = json.decode(response.body)["places"];
       Place place = Place.fromJson(tempJson);
+      _markers.add(Place.toMarker(place));
       res.add(LatLng(place.location.lat, place.location.lng));
     });
     return res;
+  }
+
+  // There is a limit on googleMaps destinations (iirc it's 25?) but
+  // I don't think that we will have more than 25 destinations to calculate
+  Future<List<LatLng>> _getPolylinePoints() async {
+    PolylinePoints points = PolylinePoints();
+    List<LatLng> coords = <LatLng>[];
+    PolylineResult res = await points.getRouteBetweenCoordinates(
+      dotenv.env["GOOGLE_MAPS_API_KEY"] as String,
+      PointLatLng(
+          _currentUserPosition.latitude, _currentUserPosition.longitude),
+    );
+    return <LatLng>[];
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      body: GoogleMap(
-          initialCameraPosition:
-              CameraPosition(target: _currentUserPosition, zoom: 14.0)),
-    ));
+            body: GoogleMap(
+                initialCameraPosition:
+                    CameraPosition(target: _currentUserPosition, zoom: 14.0),
+                markers: _markers,
+                onMapCreated: (mapController) {
+                  _mapsController.complete(mapController);
+                })));
   }
 }
