0829a7ac6141da4844afec298123fa65b432fc04
Tommaso <soncintommaso@gmail.com>
2024-01-03 17:38:55 +0100

    dump

--- lib/pages/travel_page.dart
+++ lib/pages/travel_page.dart
@@ -18,32 +18,49 @@ class TravelPage extends StatefulWidget {
 
 class _TravelPageState extends State<TravelPage> {
   final Completer<GoogleMapController> _mapsController = Completer();
-  late List<LatLng> _polylineCoordinates;
-  late List<LatLng> _locations;
+  List<LatLng> _polylineCoordinates = [];
+  List<LatLng> _locations = [];
   late final LatLng _startUserPosition;
-  late location.LocationData? _currentPosition;
-  late Set<Marker> _markers;
+  location.LocationData? _currentPosition;
+  Set<Marker> _markers = {};
   @override
-  void initState() async {
+  void initState() {
+    Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((value) {
+      setState(() {
+      _startUserPosition = LatLng(value.latitude, value.longitude);});
+    });
     super.initState();
-    _startUserPosition = await _getStartingPosition();
-    _locations = await _getPlacesfromPlaceID(widget.destinationsID);
-    _polylineCoordinates = await _getPolylinePoints();
+    print("_getStartingPosition() called.");
+    _doAsyncThings();
+  }
+
+  Future<void> _doAsyncThings() async {
+    _getStartingPosition();
+    _getPlacesfromPlaceID(widget.destinationsID);
+    _getPolylinePoints();
     _getCurrentLocation();
   }
 
-  Future<LatLng> _getStartingPosition() async {
+  void _getStartingPosition() async {
     Position startUserLocation = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
-    return LatLng(startUserLocation.latitude, startUserLocation.longitude);
+    print("_getStartingPosition() called.");
+    setState(() {
+      _startUserPosition =
+          LatLng(startUserLocation.latitude, startUserLocation.longitude);
+    });
   }
 
   void _getCurrentLocation() async {
     location.Location _currentLocation = location.Location();
     _currentLocation.getLocation().then((position) {
-      // TODO: remove old position from _markers set and add new one,
-      // also reload page with setState()
-      _currentPosition = position;
+      // does it even work?
+      _markers.remove(
+          LatLng(_currentPosition!.latitude!, _currentPosition!.longitude!));
+      setState(() {
+        _currentPosition = position;
+      });
     });
     GoogleMapController mapsController = await _mapsController.future;
     _currentLocation.onLocationChanged.listen((newPosition) {
@@ -56,15 +73,14 @@ class _TravelPageState extends State<TravelPage> {
 
   /// Sets the [_locations] variable to the geographical coordinates
   /// of all the places ID given to the widget
-  Future<List<LatLng>> _getPlacesfromPlaceID(
-      List<String> destinationsID) async {
+  void _getPlacesfromPlaceID(List<String> destinationsID) async {
     String apiKey = dotenv.env["GOOGLE_MAPS_API_KEY"] as String;
     List<LatLng> res = <LatLng>[];
-    destinationsID.forEach((destinationID) async {
+    destinationsID.forEach((destination) async {
       // Actually we only need the location field but since we already have
       // a class for Places, we can reuse that.
       String url =
-          "https://places.googleapis.com/v1/places/${destinationID}?fields=name,id,formattedAddress,location,displayName&key=${apiKey}";
+          "https://places.googleapis.com/v1/places/${destination}?fields=name,id,formattedAddress,location,displayName&key=${apiKey}";
       http.Response response = await http.get(Uri.parse(url));
       if (response.statusCode != 200) {
         throw Exception(
@@ -76,12 +92,15 @@ class _TravelPageState extends State<TravelPage> {
       _markers.add(Place.toMarker(place));
       res.add(LatLng(place.location.lat, place.location.lng));
     });
-    return res;
+    setState(() {
+      _locations = res;
+    });
   }
 
   // There is a limit on googleMaps destinations (iirc it's 25?) but
-  // I don't think that we will have more than 25 destinations to calculate
-  Future<List<LatLng>> _getPolylinePoints() async {
+  // I don't think that we will ever have more than 25 destinations to calculate
+  // but as a note leave this comment here.
+  void _getPolylinePoints() async {
     final String apiKey = dotenv.env["GOOGLE_MAPS_API_KEY"] as String;
     PolylinePoints points = PolylinePoints();
     List<LatLng> coords = <LatLng>[];
@@ -105,7 +124,9 @@ class _TravelPageState extends State<TravelPage> {
         });
       }
     }
-    return coords;
+    setState(() {
+      _polylineCoordinates = coords;
+    });
   }
 
   @override
@@ -122,13 +143,14 @@ class _TravelPageState extends State<TravelPage> {
                     },
                     polylines: {
                         Polyline(
-                            polylineId: const PolylineId("route"),
+                            polylineId: const PolylineId("travel_route"),
                             points: _polylineCoordinates,
                             color: const Color(0xABCDEF),
                             width: 6)
                       })
                 : const Center(
-                    child: const Text("Wait."),
+                    child: const Text(
+                        "Something's wrong... please reload the app."),
                   )));
   }
 }
